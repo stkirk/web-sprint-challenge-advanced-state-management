f74be609ec9739353d599316e3d729baecaf906c
stkirk <shaanekirk@gmail.com>
2021-08-29 11:29:12 -0400

    tested endpoint and completed fetchSmurfs thunk for axios request and to dispatch async actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+
+export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
+export const FETCH_SMURFS_FAILURE = "FETCH_SMURFS_FAILURE";
+
+export const fetchSmurfs = () => (dispatch) => {
+  dispatch({ type: FETCH_SMURFS_START });
+
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then((res) => {
+      console.log("RES: ", res);
+      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+    })
+    .catch((err) => {
+      console.log("ERR: ", err);
+      dispatch({ type: FETCH_SMURFS_FAILURE, payload: err });
+    });
+};
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
